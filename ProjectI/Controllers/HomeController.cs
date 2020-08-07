364c477c4353134e94401f3343cb8ed05ac143c1
Gary <66178774+gen8787@users.noreply.github.com>
2020-08-07 13:42:26 -0700

    Init commit

---
+++ ProjectI/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectI.Controllers
+{
+    public class HomeController : Controller
+    {
+        //Requests
+        //localhoset:5000/
+        [HttpGet("")]
+        public string Index()
+        {
+            return "This is my Index!";
+        }
+
+        //localhoset:5000/projects
+        [HttpGet("/projects")]
+        public string Proj()
+        {
+            return "These are my projects.";
+        }
+
+        [HttpGet("/contact")]
+        public string Cont()
+        {
+            return "This is my contact!";
+        }
+
+        //localhost:5000/users/???
+        [HttpGet("users/{username}")]
+        public string HelloUser(string username)
+        {
+            return $"Hello, {username}";
+        }
+    }
+}
